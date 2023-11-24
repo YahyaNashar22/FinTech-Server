384596a27823131d0e66220b4066e0eb7dd30006
YahyaNashar22 <cenations.people@live.com>
2023-11-24 10:41:35 +0200

    fixed transactions routes

--- routes/TransactionsRoutes.js
+++ routes/TransactionsRoutes.js
@@ -4,25 +4,19 @@ import express from "express";
 
 import {
   createTransaction,
-    getAllTransactions,
-    getTransactionById,
-    updateTransactionById,
-    deleteTransactionById,
-  } from '../controllers/TransactionController.js';
-  
+  getAllTransactions,
+  getTransactionById,
+  updateTransactionById,
+  deleteTransactionById,
+} from "../controllers/TransactionController.js";
 
+const router = express.Router();
 
-  const trasactionrouter = express.Router();
+// Use the functions in your routes or wherever needed
+router.post("/create", createTransaction);
+router.get("/read", getAllTransactions);
+router.get("/read/:id", getTransactionById);
+router.put("/update/:id", updateTransactionById);
+router.delete("/delete/:id", deleteTransactionById);
 
-  // Use the functions in your routes or wherever needed
-  app.post('/transactions/create', createTransaction);
-  app.get('/transactions/read', getAllTransactions);
-  app.get('/transactions/read/:id', getTransactionById);
-  app.put('/transactions/update/:id', updateTransactionById);
-  app.delete('/transactions/delete/:id', deleteTransactionById);
-  
-
-
-  
-  
-  export default trasactionrouter;
+export default router;
--- server.js
+++ server.js
@@ -4,17 +4,17 @@ import cors from "cors";
 import Users from "./models/users.js";
 import Transactions from "./models/transactions.js";
 import sequelize, { connect } from "../FinTech-Server/config/dbconnection.js";
-import trasactionrouter from "./routes/TransactionsRoutes.js";
 dotenv.config();
 
 import UserRoutes from "./routes/UsersRoutes.js";
+import TransactionsRoutes from "./routes/TransactionsRoutes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/users", UserRoutes);
-app.use("/transactions", trasactionrouter);
+app.use("/transactions", TransactionsRoutes);
 
 // syncronise();
 app.listen(process.env.PORT, () => {
@@ -22,14 +22,14 @@ app.listen(process.env.PORT, () => {
   console.log(`server is running on port ${process.env.PORT}`);
 });
 
-Users.hasMany(Transactions, { foreignKey: "UserID" });
-// Categories.hasMany(Transactions , { foreignKey: 'CategoriesId' });
+// Users.hasMany(Transactions, { foreignKey: "UserID" });
+// // Categories.hasMany(Transactions , { foreignKey: 'CategoriesId' });
 
-sequelize
-  .sync({ force: true })
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// sequelize
+//   .sync({ force: true })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
