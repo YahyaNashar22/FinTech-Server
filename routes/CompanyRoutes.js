1f7ae72946a3db439d112f7af313ec0c99154f7c
Rachwan <rachwan.harb2023@gmail.com>
2023-12-12 09:32:18 +0200

    Finished Edits

--- routes/CompanyRoutes.js
+++ routes/CompanyRoutes.js
@@ -10,32 +10,30 @@ import { authorized, checkRole } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.post("/create", 
-// authorized, 
-// checkRole(["admin"]), 
-uploadImage.single("Logo"),
-createCompanyInfo); // Route To create the company info
+router.post(
+  "/create",
+  authorized,
+  checkRole(["admin"]),
+  uploadImage.single("Logo"),
+  createCompanyInfo
+); // Route To create the company info
 
 router.get(
   "/info",
-  // authorized,
-  // checkRole(["admin"]),
+  authorized,
+  checkRole(["admin"]),
   uploadImage.single("Logo"),
   getCompanyInfo
 ); // Route to get the company Info
 
 router.put(
   "/update",
-  // authorized,
-  // checkRole(["admin"]),
+  authorized,
+  checkRole(["admin"]),
   uploadImage.single("Logo"),
   updateCompanyInfo
 ); // Route to update the company Info
 
-router.delete(
-  "/delete",
-  // authorized, checkRole(["admin"]),
-  deleteCompanyInfo
-);
+router.delete("/delete", authorized, checkRole(["admin"]), deleteCompanyInfo);
 
 export default router;
